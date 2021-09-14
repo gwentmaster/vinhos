802d7077c2de3afdbf49d818a9a232d112916745
gwentmaster <gwentmaster@vivaldi.net>
2021-09-14 09:11:39 +0800

    [game]: 贩卖区域模型

---
+++ vinhos/Map/SellArea.gd
@@ -0,0 +1,14 @@
+extends Panel
+class_name SellArea
+
+
+var quality_limits = [
+	[3, 4, 3, 4],
+	[5, 6, 7, 5, 6, 7],
+	[8, 9, 10, 8, 9, 10]
+]
+var color_limits = [
+	[1, 1, 0, 0],
+	[1, 1, 1, 0, 0, 0],
+	[1, 1, 1, 0, 0, 0]
+]
